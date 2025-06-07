f871804ff8373fb16228292e136f9b877e09e441
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-06-08 09:55:43 +1000

    1 Solution

---
+++ problems/1672.py
@@ -0,0 +1,26 @@
+from tools import timer
+TEST_CASES = [
+    [[1,2,3],[3,2,1]],
+    [[1,5],[7,3],[3,5]],
+    [[2,8,7],[7,1,3],[1,9,5]]
+]
+
+
+
+# TIME: O(m * n) | m = len(accounts) and n = len(account)
+# MEM: O(1)
+@timer()
+def A(accounts: list[list[int]]) -> int:
+    res = 0
+    for account in accounts:
+        cur = sum(account)
+        if cur > res: res = cur
+    return res
+
+
+
+if __name__ == "__main__":
+    for i, test in enumerate(TEST_CASES):
+        print(f'TEST {i+1} - {test}')
+        A(test)
+        print()
