390f70acf7e4256a6fe5953edef21d2e82af9ac3
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-06-08 09:06:42 +1000

    1 Solution

---
+++ problems/2236.py
@@ -0,0 +1,28 @@
+from tools import timer
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    def __str__(self): return f"[{self.val},{self.left},{self.right}]"
+
+TEST_CASES = [
+    TreeNode(10,4,6),
+    TreeNode(5,3,1)
+]
+
+
+
+# TIME: O(1)
+# MEM: O(1)
+@timer()
+def A(root: TreeNode) -> bool:
+    return root.val == root.left + root.right
+
+
+
+if __name__ == "__main__":
+    for i, test in enumerate(TEST_CASES):
+        print(f'TEST {i+1} - {test}')
+        A(test)
+        print()
