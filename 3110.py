9992e231089e34b82972bd6fbc951924ff3ba8a7
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-06-07 17:48:27 +1000

    1 Solution

---
+++ 3110.py
@@ -0,0 +1,26 @@
+import os
+from tools import timer
+
+TEST_CASES = [
+    "hello",
+    "zaz"
+]
+
+
+
+# TIME: O(n)
+# MEM: O(1)
+@timer
+def A(s: str) -> int:
+    score = 0
+    for i in range(1, len(s)): score += abs(ord(s[i-1]) - ord(s[i]))
+    return score
+
+
+
+if __name__ == "__main__":
+    os.system('cls')
+    for i, test in enumerate(TEST_CASES):
+        print(f'TEST {i+1} - {test}')
+        A(test)
+        print()
