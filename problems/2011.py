2e159c06bffc7a413bfa17408a589010c2927a59
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-06-07 22:21:01 +1000

    1 Solution

---
+++ problems/2011.py
@@ -0,0 +1,26 @@
+from tools import timer
+TEST_CASES = [
+    ["--X","X++","X++"],
+    ["++X","++X","X++"],
+    ["X++","++X","--X","X--"]
+]
+
+
+
+# TIME: O(n)
+# MEM: O(1)
+@timer()
+def A(operations: list[str]) -> int:
+    x = 0
+    for op in operations:
+        if op[1] == "+": x += 1
+        else: x -= 1
+    return x
+
+
+
+if __name__ == "__main__":
+    for i, test in enumerate(TEST_CASES):
+        print(f'TEST {i+1} - {test}')
+        A(test)
+        print()
