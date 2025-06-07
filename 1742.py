0f9d3cca3f4555b4d018d8e87c3483b97c806ca5
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-06-07 16:51:20 +1000

    Three correct O(n + log H) solutions

---
+++ 1742.py
@@ -0,0 +1,57 @@
+import os
+from tools import timer
+from collections import defaultdict
+
+TEST_CASES = [
+    (1,10),
+    (5,15),
+    (19,28),
+    (1,100000)
+]
+
+
+
+# TIME: O(n + log H) | H = highLimit
+# MEM: O(n)
+@timer
+def A(lowLimit: int, highLimit: int) -> int:
+    res = defaultdict(lambda: 0)
+    for num in [sum(map(int, str(num))) for num in range(lowLimit, highLimit + 1)]: res[num] += 1
+    return max(res.values())
+
+# TIME: O(n + log H) | More performant by avoiding str conversions
+# MEM: O(n)
+@timer
+def B(lowLimit: int, highLimit: int) -> int:
+    def digitSum(n):
+        s = 0
+        while n: s, n = s + n % 10, n // 10
+        return s
+    
+    res = defaultdict(lambda: 0)
+    for num in [digitSum(num) for num in range(lowLimit, highLimit + 1)]: res[num] += 1
+    return max(res.values())
+
+# TIME: O(n + log H) | Same as B
+# MEM: O(log H)
+@timer
+def C(lowLimit: int, highLimit: int) -> int:
+    def digitSum(n):
+        s = 0
+        while n: s, n = s + n % 10, n // 10
+        return s
+    
+    res = defaultdict(lambda: 0)
+    for num in range(lowLimit, highLimit + 1): res[digitSum(num)] += 1
+    return max(res.values())
+
+
+
+if __name__ == "__main__":
+    os.system('cls')
+    for test in TEST_CASES:
+        print(f'TEST 1 - {test}')
+        A(*test)
+        B(*test)
+        C(*test)
+        print()
