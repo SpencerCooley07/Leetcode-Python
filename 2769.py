4439a54af263eb05ea104c206fa66857504b0c27
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-06-07 17:56:50 +1000

    1 solution

---
+++ 2769.py
@@ -0,0 +1,21 @@
+from tools import timer
+TEST_CASES = [
+    (4, 1),
+    (3, 2)
+]
+
+
+
+# TIME: O(1)
+# MEM: O(1)
+@timer
+def A(num: int, t: int) -> int:
+    return num + 2 * t
+
+
+
+if __name__ == "__main__":
+    for i, test in enumerate(TEST_CASES):
+        print(f'TEST {i+1} - {test}')
+        A(*test)
+        print()
