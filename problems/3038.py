f1cb0d26c4fe15fa97e8acb1e0e5348a5c6abee6
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-06-08 15:43:13 +1000

    Maximum Number of Operations With the Same Score I

--- problems/3038.py
+++ problems/3038.py
@@ -13,7 +13,7 @@ TEST_CASES = [
 def A(nums: list[int]) -> int:
     res, score = 0, nums[0] + nums[1]
     for i in range(1, len(nums), 2):
-        if nums[i-1] + nums[i] == score: res += 1
+        if (nums[i-1] + nums[i]) == score: res += 1
         else: break
     return res
 
