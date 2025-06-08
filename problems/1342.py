2f15e4165d27a991ecdccf1434d136831734548a
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-06-08 10:34:01 +1000

    Number of Steps to Reduce a Number to Zero

--- problems/1342.py
+++ problems/1342.py
@@ -18,10 +18,22 @@ def A(num: int) -> int:
         count += 1
     return count
 
+# TIME: O(log n)
+# MEM: O(1)
+@timer() # Alternative bitwise solution (slightly faster)
+def B(num: int) -> int:
+    count = 0
+    while num > 0:
+        if num & 1 == 0: num >>= 1
+        else: num -= 1
+        count += 1
+    return count
+
 
 
 if __name__ == "__main__":
     for i, test in enumerate(TEST_CASES):
         print(f'TEST {i+1} - {test}')
         A(test)
+        B(test)
         print()
